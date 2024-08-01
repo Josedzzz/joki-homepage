26e88518edc11d31e47ecbb5cc59d84779581081
joki <josedavidamayar@gmail.com>
2024-07-31 21:04:03 -0500

    Add onclick event for each link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,8 @@
-export default function Header() {
+import PropTypes from "prop-types";
+
+export default function Header(props) {
+  const { toggleWindow } = props;
+
   return (
     <header className="flex justify-between items-center p-4 mx-auto max-w-3xl gap-4">
       <div className="flex items-center space-x-2">
@@ -6,19 +10,36 @@ export default function Header() {
         <h2 className="text-4xl text-white font-bold">Joki</h2>
       </div>
       <nav className="space-x-5">
-        <a href="#" className="text-white hover:text-blue-400 font-bold">
+        <a
+          onClick={() => toggleWindow("home")}
+          className="text-white hover:text-blue-400 font-bold"
+        >
           <i className="fa-solid fa-house"></i> Home
         </a>
-        <a href="#" className="text-white hover:text-blue-400 font-bold">
+        <a
+          onClick={() => toggleWindow("about-me")}
+          className="text-white hover:text-blue-400 font-bold"
+        >
           <i className="fa-solid fa-user"></i> About me
         </a>
-        <a href="#" className="text-white hover:text-blue-400 font-bold">
-          <i className="fa-solid fa-keyboard"></i> Works
+        <a
+          onClick={() => toggleWindow("project")}
+          className="text-white hover:text-blue-400 font-bold"
+        >
+          <i className="fa-solid fa-keyboard"></i> Projects
         </a>
-        <a href="#" className="text-white hover:text-blue-400 font-bold">
+        <a
+          href="https://github.com/Josedzzz"
+          className="text-white hover:text-blue-400 font-bold"
+          target="_blank"
+        >
           <i className="fa-brands fa-github"></i> Source
         </a>
       </nav>
     </header>
   );
 }
+
+Header.propTypes = {
+  toggleWindow: PropTypes.func.isRequired,
+};
