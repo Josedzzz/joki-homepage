1deee7e09268c43a31ad78fdab76207705495dfd
joki <josedavidamayar@gmail.com>
2024-08-01 03:59:29 -0500

    call the components in different files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
-import PropTypes from "prop-types";
+import Findme from "./Findme";
+import SkillPanel from "./SkillPanel";
 
 export default function Home() {
   const handleCVDownload = () => {
@@ -35,69 +36,8 @@ export default function Home() {
           className="w-28 h-28 rounded-full border-4 border-blue-400 mb-4"
         />
       </div>
+      <SkillPanel />
       <Findme />
     </main>
   );
 }
-
-const Findme = () => {
-  return (
-    <div className="flex flex-col gap-6">
-      <h2 className="text-xl sm:text-xl md:text-2xl text-blue-400 font-bold border-b-2 border-blue-400 ">
-        On the web
-      </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        <SocialMedia
-          icon={<i className="fa-brands fa-github"></i>}
-          title={"@Josedzzz"}
-          reference={"https://github.com/Josedzzz"}
-        />
-        <SocialMedia
-          icon={<i className="fa-solid fa-code"></i>}
-          title={"@JokiNP"}
-          reference={"https://leetcode.com/u/JokiNP/"}
-        />
-        <SocialMedia
-          icon={<i className="fa-brands fa-linkedin"></i>}
-          title={"@Jose David Amaya"}
-          reference={"https://www.linkedin.com/in/jose-david-amaya-37ba23167/"}
-        />
-        <SocialMedia
-          icon={<i className="fa-brands fa-instagram"></i>}
-          title={"@jokidzz"}
-          reference={"https://www.instagram.com/jokidzz/"}
-        />
-      </div>
-    </div>
-  );
-};
-
-const SocialMedia = (props) => {
-  const { icon, title, reference } = props;
-
-  const handleClick = () => {
-    if (reference.startsWith("https://")) {
-      window.open(reference, "_blank");
-    } else {
-      navigator.clipboard.writeText(title).then(() => {
-        alert(`Copied ${title} to clipboard`);
-      });
-    }
-  };
-
-  return (
-    <button
-      onClick={handleClick}
-      className="flex items-center space-x-2 p-2 border-2 border-white rounded-xl hover:bg-blue-400 transition duration-300 ease-in-out transform hover:scale-105"
-    >
-      <div className="text-white text-lg">{icon}</div>
-      <span className="text-lg text-white">{title}</span>
-    </button>
-  );
-};
-
-SocialMedia.propTypes = {
-  icon: PropTypes.element.isRequired,
-  title: PropTypes.string.isRequired,
-  reference: PropTypes.string.isRequired,
-};
