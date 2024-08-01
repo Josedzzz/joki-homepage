ec70198897a1f075bcff000996410f6a3212c5b3
joki <josedavidamayar@gmail.com>
2024-08-01 03:58:43 -0500

    array of socials

---
+++ src/utils/socials.js
@@ -0,0 +1,22 @@
+export const socials = [
+  {
+    icon: "fa-brands fa-github",
+    title: "@Josedzzz",
+    reference: "https://github.com/Josedzzz",
+  },
+  {
+    icon: "fa-solid fa-code",
+    title: "@JokiNP",
+    reference: "https://leetcode.com/u/JokiNP/",
+  },
+  {
+    icon: "fa-brands fa-linkedin",
+    title: "@Jose David Amaya",
+    reference: "https://www.linkedin.com/in/jose-david-amaya-37ba23167/",
+  },
+  {
+    icon: "fa-brands fa-instagram",
+    title: "@jokidzz",
+    reference: "https://www.instagram.com/jokidzz/",
+  },
+];
