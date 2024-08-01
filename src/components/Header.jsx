1af88e5cdec2e9a7d17b3ffb40247abcc523568b
joki <josedavidamayar@gmail.com>
2024-08-01 00:09:45 -0500

    Make the header content responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,31 +6,33 @@ export default function Header(props) {
   return (
     <header className="flex justify-between items-center p-4 mx-auto max-w-3xl gap-4">
       <div className="flex items-center space-x-2">
-        <i className="fa-solid fa-terminal text-blue-400 text-2xl"></i>
-        <h2 className="text-4xl text-white font-bold">Joki</h2>
+        <i className="fa-solid fa-terminal text-blue-400 text-lg sm:text-xl md:text-2xl"></i>
+        <h2 className="text-xl sm:text-3xl md:text-4xl text-white font-bold">
+          Joki
+        </h2>
       </div>
       <nav className="space-x-5">
         <a
           onClick={() => toggleWindow("home")}
-          className="text-white hover:text-blue-400 font-bold"
+          className="text-white hover:text-blue-400 font-bold text-sm sm:text-md md:text-lg"
         >
           <i className="fa-solid fa-house"></i> Home
         </a>
         <a
           onClick={() => toggleWindow("about-me")}
-          className="text-white hover:text-blue-400 font-bold"
+          className="text-white hover:text-blue-400 font-bold text-sm sm:text-md md:text-lg"
         >
           <i className="fa-solid fa-user"></i> About me
         </a>
         <a
           onClick={() => toggleWindow("project")}
-          className="text-white hover:text-blue-400 font-bold"
+          className="text-white hover:text-blue-400 font-bold text-sm sm:text-md md:text-lg"
         >
           <i className="fa-solid fa-keyboard"></i> Projects
         </a>
         <a
           href="https://github.com/Josedzzz"
-          className="text-white hover:text-blue-400 font-bold"
+          className="text-white hover:text-blue-400 font-bold text-sm sm:text-md md:text-lg"
           target="_blank"
         >
           <i className="fa-brands fa-github"></i> Source
