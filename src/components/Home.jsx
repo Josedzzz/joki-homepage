58a0d82a91e5f7b9e4d940172cf4a02b765bbad1
joki <josedavidamayar@gmail.com>
2024-08-01 00:10:34 -0500

    Edit text and an image about me

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,24 @@
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main className="flex flex-col gap-9 p-4 mx-auto max-w-2xl">
+      <div className="flex justify-between items-center">
+        <div className="flex flex-col">
+          <h1 className="text-xl sm:text-3xl md:text-4xl text-white font-bold">
+            Hello, I'm
+          </h1>
+          <span className="text-xl sm:text-3xl md:text-4xl text-blue-400 font-bold">
+            José David Amaya
+          </span>
+          <h2 className="text-lg sm:text-xl md:text-2xl text-white">
+            Digital Craftsman ( Developer )
+          </h2>
+        </div>
+        <img
+          src="/images/me.jpeg"
+          alt="José David Amaya"
+          className="w-28 h-28 rounded-full border-4 border-blue-400 mb-4"
+        />
+      </div>
+    </main>
+  );
 }
