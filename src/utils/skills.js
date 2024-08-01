7e5f72f7365f5ae5c3a07e974776330bd3b8ce64
joki <josedavidamayar@gmail.com>
2024-08-01 03:58:11 -0500

    array of skilss

---
+++ src/utils/skills.js
@@ -0,0 +1,14 @@
+// src/utils/skills.js
+export const skills = [
+  { icon: "fa-brands fa-java", title: "Java" },
+  { icon: "fa-brands fa-html5", title: "HTML" },
+  { icon: "fa-brands fa-css3-alt", title: "CSS" },
+  { icon: "fa-brands fa-js", title: "JavaScript" },
+  { icon: "fa-brands fa-react", title: "React" },
+  { icon: "fa-brands fa-node-js", title: "NodeJs" },
+  { icon: "fa-brands fa-css3", title: "TailwindCSS" },
+  { icon: "fa-brands fa-linux", title: "Linux" },
+  { icon: "fa-brands fa-docker", title: "Docker" },
+  { icon: "fa-brands fa-git-alt", title: "Git" },
+  { icon: "fa-brands fa-python", title: "Python" },
+];
