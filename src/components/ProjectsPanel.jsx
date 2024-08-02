475000f125ed3dd5100a1d4ad1160274284eb7c3
joki <josedavidamayar@gmail.com>
2024-08-02 00:06:20 -0500

    new component that contains all the projects

---
+++ src/components/ProjectsPanel.jsx
@@ -0,0 +1,25 @@
+import { projects } from "../utils/projects";
+import Project from "./Project";
+
+export default function ProjectsPanel() {
+  return (
+    <main className="flex flex-col gap-10 p-4 mx-auto max-w-xl">
+      <h2 className="text-xl sm:text-xl md:text-2xl text-blue-400 font-bold">
+        Projects and Collaborations
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        {projects.map((project, index) => {
+          return (
+            <Project
+              key={index}
+              image={project.image}
+              title={project.title}
+              text={project.text}
+              reference={project.reference}
+            />
+          );
+        })}
+      </div>
+    </main>
+  );
+}
