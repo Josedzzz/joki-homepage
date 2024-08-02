dbe5f649aacb49ee12e2a917685c018098e1bf08
joki <josedavidamayar@gmail.com>
2024-08-01 21:00:55 -0500

    add bio content for the aboutme comp

---
+++ src/utils/bio.js
@@ -0,0 +1,13 @@
+export const bio = [
+  { date: "2005", description: "Born in Armenia, Colombia" },
+  {
+    date: "2021",
+    description:
+      "I obtained my bilingual academic bachelor's degree at the Policarpa School.",
+  },
+  {
+    date: "2022",
+    description:
+      "I started studying systems engineering at the Universidad del Quindio.",
+  },
+];
