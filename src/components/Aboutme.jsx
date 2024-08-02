1b91243faee79d05ef00ab4e8b4aa06a262427d6
joki <josedavidamayar@gmail.com>
2024-08-01 21:03:13 -0500

    Render the findme, intro and bio components

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,3 +1,13 @@
+import Bio from "./Bio.jsx";
+import Findme from "./Findme.jsx";
+import Intro from "./Intro.jsx";
+
 export default function Aboutme() {
-  return <div>About me</div>;
+  return (
+    <main className="flex flex-col gap-3 p-4 mx-auto max-w-xl">
+      <Findme />
+      <Intro />
+      <Bio />
+    </main>
+  );
 }
