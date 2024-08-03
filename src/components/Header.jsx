0388cdee3af5ad53081f332a2ea6a36134710d8a
joki <josedavidamayar@gmail.com>
2024-08-02 19:12:04 -0500

    make the header fixed with blur

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,37 +11,42 @@ export default function Header(props) {
     }`;
 
   return (
-    <header className="flex justify-between items-center p-2 mx-auto max-w-3xl gap-4 appearr">
-      <div className="flex items-center space-x-1">
-        <i className="fa-solid fa-terminal text-blue-400 text-lg sm:text-xl md:text-2xl"></i>
-        <h2 className="text-xl sm:text-3xl md:text-4xl text-white font-bold">
-          Joki
-        </h2>
+    <header className="fixed h-12 w-full bg-custom-dark-translucent backdrop-blur z-50">
+      <div className="flex justify-between items-center p-2 mx-auto max-w-3xl gap-4">
+        <div className="flex items-center space-x-1">
+          <i className="fa-solid fa-terminal text-blue-400 text-lg sm:text-xl md:text-2xl"></i>
+          <h2 className="text-xl sm:text-3xl md:text-4xl text-white font-bold">
+            Joki
+          </h2>
+        </div>
+        <nav className="space-x-4 sm:space-x-7">
+          <a
+            onClick={() => toggleWindow("home")}
+            className={linkClasses("home")}
+          >
+            <i className="fa-solid fa-house"></i> Home
+          </a>
+          <a
+            onClick={() => toggleWindow("about-me")}
+            className={linkClasses("about-me")}
+          >
+            <i className="fa-solid fa-user"></i> About me
+          </a>
+          <a
+            onClick={() => toggleWindow("project")}
+            className={linkClasses("project")}
+          >
+            <i className="fa-solid fa-keyboard"></i> Projects
+          </a>
+          <a
+            href="https://github.com/Josedzzz"
+            className="text-white hover:text-blue-400 font-bold text-sm sm:text-md md:text-lg"
+            target="_blank"
+          >
+            <i className="fa-brands fa-github"></i>
+          </a>
+        </nav>
       </div>
-      <nav className="space-x-4 sm:space-x-7">
-        <a onClick={() => toggleWindow("home")} className={linkClasses("home")}>
-          <i className="fa-solid fa-house"></i> Home
-        </a>
-        <a
-          onClick={() => toggleWindow("about-me")}
-          className={linkClasses("about-me")}
-        >
-          <i className="fa-solid fa-user"></i> About me
-        </a>
-        <a
-          onClick={() => toggleWindow("project")}
-          className={linkClasses("project")}
-        >
-          <i className="fa-solid fa-keyboard"></i> Projects
-        </a>
-        <a
-          href="https://github.com/Josedzzz"
-          className="text-white hover:text-blue-400 font-bold text-sm sm:text-md md:text-lg"
-          target="_blank"
-        >
-          <i className="fa-brands fa-github"></i>
-        </a>
-      </nav>
     </header>
   );
 }
