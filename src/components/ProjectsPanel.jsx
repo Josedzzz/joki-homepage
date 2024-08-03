457f9d353ecb18b856182c45799c5fc354d852d3
joki <josedavidamayar@gmail.com>
2024-08-02 19:11:20 -0500

    Add margin top

--- src/components/ProjectsPanel.jsx
+++ src/components/ProjectsPanel.jsx
@@ -4,7 +4,7 @@ import Project from "./Project";
 export default function ProjectsPanel() {
   return (
     <main className="flex flex-col gap-10 p-4 mx-auto max-w-xl appear-bottom">
-      <h2 className="text-xl sm:text-xl md:text-2xl text-blue-400 font-bold">
+      <h2 className="mt-12 text-xl sm:text-xl md:text-2xl text-blue-400 font-bold">
         Projects and Collaborations
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
